daeb68165129bd063501347a4d89ee2fe3d405ab
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-02-20 12:32:14 +0600

    created express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +15,44 @@ app.use(express.json()); // Parse JSON requests
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
+
+// Mongo db
+
+const uri = process.env.MONGODB_URI_COMPASS
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    const userCollection = client.db("zenTaskDb").collection("users")
+
+    app.post("/users", async (req, res) => {
+      const user = {name:"hello"}
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Start the server
 app.listen(PORT, () => {
